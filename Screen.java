03f7aeb7cdf1c6e9adf3f9379a97d357c24b1002
wonez <ffazlic@yahoo.com>
2018-01-02 14:56:18 +0100

    Buttons

--- Screen.java
+++ Screen.java
@@ -1,7 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Screen extends JFrame {
+
+    private JButton minusCol;
+    private JButton plusCol;
+
+    private JButton minusRow;
+    private JButton plusRow;
+
+    private ArrayList<JTextField> inputs;
 
     public Screen() {
         setSize(400, 400);
@@ -10,22 +19,30 @@ public class Screen extends JFrame {
         setLocationRelativeTo(null);
 
         JPanel nums = new JPanel(new GridLayout(3,3));
+        inputs = new ArrayList<>(9);
 
         for(int i=0; i<9; i ++) {
             JTextField jtx = new JTextField();
+            inputs.add(jtx);
             nums.add(jtx);
         }
 
         add(nums, BorderLayout.CENTER);
 
-        JPanel bottom = new JPanel(new GridLayout(3,3));
-        bottom.add(new JButton("+"));
+        JPanel columns = new JPanel(new GridLayout(1,3));
+        minusCol = new JButton("-");
+        plusCol = new JButton("+");
+        columns.add(minusCol);
+        columns.add(new JLabel("Columns", SwingConstants.CENTER));
+        columns.add(plusCol);
+        add(columns, BorderLayout.NORTH);
 
-        add(bottom, BorderLayout.SOUTH);
-
-        JPanel right = new JPanel(new GridLayout(3, 3));
-        right.add(new JButton("+"));
-
-        add(right, BorderLayout.EAST);
+        JPanel rows = new JPanel(new GridLayout(1,3));
+        minusRow = new JButton("-");
+        plusRow = new JButton("+");
+        rows.add(minusRow);
+        rows.add(new JLabel("Rows", SwingConstants.CENTER));
+        rows.add(plusRow);
+        add(rows, BorderLayout.SOUTH);
     }
 }
