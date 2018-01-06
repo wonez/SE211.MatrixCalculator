930367947a344c34acb7b4e0789edcae23818ddd
wonez <ffazlic@yahoo.com>
2018-01-06 18:25:41 +0100

    layout modified

--- Screen.java
+++ Screen.java
@@ -30,6 +30,12 @@ public class Screen extends JFrame implements ActionListener {
 
     public Screen(int ar, int ac, int br, int bc) {
 
+        setSize(750, 250);
+        setLayout(new GridLayout(1, 1));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Matrix Calculator");
+        setLocationRelativeTo(null);
+
         //A matrix row and column
         this.ar = ar;
         this.ac = ac;
@@ -37,12 +43,10 @@ public class Screen extends JFrame implements ActionListener {
         //B matrix row and column
         this.br = br;
         this.bc = bc;
-        setSize(800, 400);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("Matrix Calculator");
-        setLocationRelativeTo(null);
 
-        matrixA = new JPanel(new GridLayout(ar,ac));
+        JPanel A = new JPanel(new BorderLayout());
+
+        matrixA = new JPanel(new GridLayout(ar, ac));
         inputsA = new ArrayList<>(ar*ac);
 
         for(int i=0; i<ar; i++) {
@@ -53,29 +57,9 @@ public class Screen extends JFrame implements ActionListener {
             }
         }
 
-        matrixB = new JPanel(new GridLayout(br, bc));
-        inputsB = new ArrayList<>(br*bc);
+        A.add(matrixA, BorderLayout.CENTER);
 
-        for(int i=0; i< br; i++) {
-            for(int j=0; j< bc; j++) {
-                JTextField jtx = new JTextField();
-                inputsB.add(jtx);
-                matrixB.add(jtx);
-            }
-        }
-
-        commands = new JPanel();
-
-        mainPanel = new JPanel(new GridLayout(1,3));
-        mainPanel.add(matrixA);
-        mainPanel.add(commands);
-        mainPanel.add(matrixB);
-
-        add(mainPanel, BorderLayout.CENTER);
-
-        JPanel bottomPanel = new JPanel();
-
-        JPanel matrixCommands = new JPanel(new GridLayout(1, 4));
+        //A commands
         JPanel aCommands = new JPanel(new GridLayout(1, 4));
 
         JButton plusA = new JButton("+");
@@ -90,7 +74,31 @@ public class Screen extends JFrame implements ActionListener {
         cleanA.addActionListener(this);
         aCommands.add(cleanA);
 
+        JButton constA = new JButton("Const");
+        constA.addActionListener(this);
+        aCommands.add(constA);
+
+        A.add(aCommands, BorderLayout.SOUTH);
+
+        //B
+        JPanel B = new JPanel(new BorderLayout());
+
+        matrixB = new JPanel(new GridLayout(br, bc));
+        inputsB = new ArrayList<>(br*bc);
+
+        for(int i=0; i< br; i++) {
+            for(int j=0; j< bc; j++) {
+                JTextField jtx = new JTextField();
+                inputsB.add(jtx);
+                matrixB.add(jtx);
+            }
+        }
+
+        B.add(matrixB, BorderLayout.CENTER);
+
+        //B commands
         JPanel bCommands = new JPanel(new GridLayout(1, 4));
+
         JButton plusB = new JButton("+");
         plusB.addActionListener(new ActionListener() {
             @Override
@@ -127,12 +135,24 @@ public class Screen extends JFrame implements ActionListener {
         });
         bCommands.add(cleanB);
 
-        matrixCommands.add(aCommands);
-        matrixCommands.add(new JPanel(new GridLayout(1,4)));
-        matrixCommands.add(bCommands);
+        JButton constB = new JButton("Const");
+        constB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("ne radi jos");
+            }
+        });
+        bCommands.add(constB);
 
-        bottomPanel.add(matrixCommands, BorderLayout.NORTH);
-        add(bottomPanel, BorderLayout.SOUTH);
+        B.add(bCommands, BorderLayout.SOUTH);
+        commands = new JPanel(new BorderLayout());
+
+        mainPanel = new JPanel(new GridLayout(1,3));
+        mainPanel.add(A);
+        mainPanel.add(commands);
+        mainPanel.add(B);
+
+        add(mainPanel);
     }
 
     @Override
@@ -152,6 +172,8 @@ public class Screen extends JFrame implements ActionListener {
             for(JTextField jtx: inputsA){
                 jtx.setText("");
             }
+        } else if (e.getActionCommand().equals("Const")){
+            System.out.println("ner adi jos");
         }
 
     }
